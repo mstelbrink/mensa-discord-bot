ccb605a9b0a5c2b8b08995dbf3114b83a6cbe2e5
Matteo Stelbrink <147656301+mstelbrink@users.noreply.github.com>
2023-10-19 08:41:30 +0200

    Refactoring

--- index.js
+++ index.js
@@ -9,30 +9,32 @@ client.once(Events.ClientReady, c => {
 
     const date = new Date();
     const day = date.getDate();
-    const month = date.getMonth() + 1; // +1 is necessary because months start at 0
+    const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const url = 'https://openmensa.org/api/v2/canteens/' + mensa_id + '/days/' + year + '-' + month + '-' + day + '/meals';
     
         if (!(date.getDay === 0 && date.getDate === 6)) {
             fetch(url)
             .then((response) => {
+
                 if (response.ok) {
                     return response.json();
                 } throw new Error('Reqest failed!');}, networkError => {
                     console.log(networkError.message);
                 })
+
             .then((responseText) => {
-    
+
                 const mealMap = new Map();
                 const categoriesMap = new Map();
                 const hasTitleMap = new Map();
-    
+
                 for (let i = 0; i < responseText.length; i++) {
                     mealMap.set(i, responseText[i].name);
                     categoriesMap.set(responseText[i].name, responseText[i].category);
                 }
-    
-                categoriesMap.forEach((value, key) => {
+
+                categoriesMap.forEach((value) => {
                     hasTitleMap.set(value, false);
                 });
     
